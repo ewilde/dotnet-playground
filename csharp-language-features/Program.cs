fa6602f8815dcc996222218e3ccdb085818a273b
ewilde <ewilde@gmail.com>
2013-12-13 15:36:52 +0000

    Expression tree sample

---
+++ csharp-language-features/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_language_features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ExpressionTrees().AddTwoNumbersExpression(10, 20);
+            Console.ReadKey();
+        }
+    }
+}
