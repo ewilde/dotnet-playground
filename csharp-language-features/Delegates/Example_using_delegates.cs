edb8d4732e6aa9a3691bde735df9456eacdd7060
Edward Wilde <ewilde@gmail.com>
2014-02-15 18:01:31 +0000

    Delegates

--- csharp-language-features/Delegates/Example_using_delegates.cs
+++ csharp-language-features/Delegates/Example_using_delegates.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace Edward.Wilde.CSharp.Features.Delegates
 {
-    internal delegate int CalculateAdd(int a, int b);
+    delegate int CalculateAdd(int a, int b);
 
+    /// <summary>
+    /// Delegates types as declared above are special types that inherit from <see cref="MulticastDelegate"/>.
+    /// Delegate instances are similar to normal reference type instances and can be passed around in the same way.
+    /// </summary>
     class Example_using_delegates
     {
         public void Run()
         {
-            
+            var calculatorDelegate = new CalculateAdd(this.Sum);
+            Console.WriteLine("Sum result using a delegate: {0}", calculatorDelegate(5,6));
+
+            CalculateAdd anotherDelegate = this.Sum;
+            Console.WriteLine(anotherDelegate.Invoke(4, 34));
         }
         public int Sum(int a, int b)
         {
