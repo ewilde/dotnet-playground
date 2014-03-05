76bbfc0bfa80edc2c611b0a895360b7822485820
Edward Wilde <ewilde@gmail.com>
2014-03-05 08:39:35 +0000

    singleton pattern lazy solution

---
+++ csharp-language-features/Singleton/SingletonPatterns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// Solutions based on Jon Skeet's article http://csharpindepth.com/Articles/General/Singleton.aspx#lazy
+namespace Edward.Wilde.CSharp.Features.Singleton
+{
+    public sealed class Singleton6
+    {
+        private static readonly Lazy<Singleton6> instance = new Lazy<Singleton6>(() => new Singleton6());
+
+        public static Singleton6 Instance { get { return instance.Value; } }
+
+        private Singleton6()
+        {
+        }
+    }
+}
