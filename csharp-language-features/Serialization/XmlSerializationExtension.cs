77aeccef5740930ad38749cd1540216dba4c57c7
Edward Wilde <ewilde@gmail.com>
2014-03-31 08:35:23 +0100

    Xml serialization utility class

---
+++ csharp-language-features/Serialization/XmlSerializationExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Edward.Wilde.CSharp.Features.Serialization
+{
+    public static class XmlSerializationExtension
+    {
+        public static string XmlSerialize<T>(this T objectToSerialize)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            var stringWriter = new StringWriter();
+            var xmlWriter = new XmlTextWriter(stringWriter)
+            {
+                Formatting = Formatting.Indented
+            };
+
+            xmlSerializer.Serialize(xmlWriter, objectToSerialize);
+
+            return stringWriter.ToString();
+        }  
+    }
+}
