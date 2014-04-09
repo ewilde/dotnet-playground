90c362ee2d335ec7664efefc5b68a5e69fbcd68c
Edward Wilde <ewilde@gmail.com>
2014-04-09 14:55:35 +0100

    Added UTF8StringWriter

---
+++ csharp-language-features/Serialization/Utf8StringWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Edward.Wilde.CSharp.Features.Serialization
+{
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+}
--- csharp-language-features/Serialization/XmlSerializationExtension.cs
+++ csharp-language-features/Serialization/XmlSerializationExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -6,11 +7,11 @@ namespace Edward.Wilde.CSharp.Features.Serialization
 {
     public static class XmlSerializationExtension
     {
-        public static string XmlSerialize<T>(this T objectToSerialize)
+        public static string XmlSerialize<T>(this T objectToSerialize, bool utf8 = true)
         {
             var xmlSerializer = new XmlSerializer(typeof(T));
 
-            var stringWriter = new StringWriter();
+            var stringWriter = utf8 ? new Utf8StringWriter() : new StringWriter(); // otherwise utf-16
             var xmlWriter = new XmlTextWriter(stringWriter)
             {
                 Formatting = Formatting.Indented
