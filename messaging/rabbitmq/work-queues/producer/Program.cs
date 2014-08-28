e11949d2d405a5dc467275d98e06b86a55051782
Edward Wilde <ewilde@gmail.com>
2014-08-28 08:52:48 +0100

    rabbit-mq work queue example

---
+++ messaging/rabbitmq/work-queues/producer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rabbitmq.workqueue.producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Send().Run(args);
+        }
+    }
+}
