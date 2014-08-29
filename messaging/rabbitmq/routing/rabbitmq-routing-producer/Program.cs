bc6b1b9f23bc52f8d7fe83f38096c566351e34ee
Edward Wilde <ewilde@gmail.com>
2014-08-29 17:56:57 +0100

    rabbitmq routing example

---
+++ messaging/rabbitmq/routing/rabbitmq-routing-producer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabbitmq.routing.producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new EmitLogDirect().Run(args);
+        }
+    }
+}
