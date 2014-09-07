77d1900610c6fb02ff5f7328258d2636cc1e98df
Edward Wilde <ewilde@gmail.com>
2014-09-07 20:46:10 +0100

    Apachemq perf test

---
+++ messaging/activemq/performance-test/activemq-perf-consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace activemq.perf.consumer
+{
+    class Program
+    {
+        private static int receivedMessages = 0;
+        private static Timer timer;
+
+        static void Main(string[] args)
+        {
+            var connecturi = new Uri("activemq:tcp://localhost:61616");
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += timer_Elapsed;
+
+            Console.WriteLine("About to connect to " + connecturi);
+
+            var factory = new Apache.NMS.ActiveMQ.ConnectionFactory(connecturi);
+            using (var connection = factory.CreateConnection())
+            using (var session = connection.CreateSession())
+            {
+                connection.Start();
+                IDestination destination = session.GetTopic("perf");
+                using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                {
+                    while (true)
+                    {
+                        timer.Start();
+                        var message = consumer.Receive();
+                        receivedMessages += 1;
+                        //Console.WriteLine(message);
+                    }
+                }
+            }
+        }
+
+        private static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("{0} messages per second received.", receivedMessages);
+            receivedMessages = 0;
+        }
+    }
+}
---
+++ messaging/activemq/performance-test/activemq-perf-producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom;
+using System.Diagnostics;
+using Apache.NMS;
+using Edward.Wilde.CSharp.Features.Strings;
+using Edward.Wilde.CSharp.Features.Utilities;
+
+namespace activemq.perf.producer
+{
+    class Program
+    {
+        static readonly string Message = StringExtensions.RandomString(4.Kilobytes());
+
+        static void Main(string[] args)
+        {
+            var connecturi = new Uri("activemq:tcp://localhost:61616");
+
+            Console.WriteLine("About to connect to " + connecturi);
+
+            var factory = new Apache.NMS.ActiveMQ.ConnectionFactory(connecturi);
+            using (var connection = factory.CreateConnection())
+            using (var session  = connection.CreateSession())
+            {
+                IDestination destination = session.GetTopic("perf");
+                using (IMessageProducer producer = session.CreateProducer(destination))
+                {
+                    int sendMessages = 0;
+                    var watch = new Stopwatch();
+                    watch.Start();
+                    while (true)
+                    {
+                        ITextMessage message = session.CreateTextMessage(Message);
+                        producer.Send(message);
+                        sendMessages += 1;
+                        if (watch.ElapsedMilliseconds > 1000)
+                        {
+                            Console.WriteLine("{0} messages per second sent.", sendMessages);
+                            watch.Restart();
+                            sendMessages = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
