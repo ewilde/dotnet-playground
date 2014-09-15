fd10295ec099f93d5d0e606861ca7ba4143fa48a
Edward Wilde <ewilde@gmail.com>
2014-09-15 16:15:41 +0100

    tibrv consumer

--- messaging/tibrv/performance-test/tibrv-perf-consumer/Program.cs
+++ messaging/tibrv/performance-test/tibrv-perf-consumer/Program.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using TIBCO.Rendezvous;
 
 namespace tibrv_perf_consumer
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int receivedMessages;
+        private static Timer timer;
+
+        private static void Main(string[] args)
         {
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+            new Program().Run();
+            Console.WriteLine("Waiting for messages....");
+            Console.ReadKey();
+        }
+
+        public void Run()
+        {
+            var service = "7500";
+            var network = ";239.255.0.1";
+            var daemon = "tcp:7500";
+            var subject = "TEST.Perf";
+            receivedMessages = 0;
+
+            TIBCO.Rendezvous.Environment.Open();
+            var transport = new NetTransport(service, network, daemon);
+            var listener = new Listener(Queue.Default, OnMessageReceived, transport, subject, null);
+            var dispacher = new Dispatcher(listener.Queue);       
+            dispacher.Join();
+        }
+
+        private void OnMessageReceived(object listener, MessageReceivedEventArgs args)
+        {
+            var body = args.Message.GetField("body").Value as byte[];
+            var message = Encoding.UTF8.GetString(body);
+            receivedMessages += 1;
+        }
+        
+        private static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("{0} messages per second received.", receivedMessages);
+            receivedMessages = 0;
         }
     }
 }
