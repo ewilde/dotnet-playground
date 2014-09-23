5afb8ada560d869a23e45430f5e54627a81b17e7
Edward Wilde <ewilde@gmail.com>
2014-09-23 20:51:37 +0100

    zeromq pub sub sample

---
+++ messaging/zeromq/performance-test/zeromq-perf-consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Timers;
+using NetMQ;
+
+namespace zeromq.perf.consumer
+{
+    class Program
+    {
+        private static int receivedMessages = 0;
+        private static Timer timer;
+
+        static void Main(string[] args)
+        {
+            const string address = "ipc://127.0.0.1:5556";
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += timer_Elapsed;
+
+            Console.WriteLine("About to connect to " + address);
+
+            using (var context = NetMQContext.Create())
+            using (var subscriber = context.CreateSubscriberSocket())
+            {
+                subscriber.Connect(address);
+                timer.Start();
+                subscriber.Subscribe("");
+                while (true)
+                {
+                    var message = subscriber.ReceiveString();
+                    receivedMessages += 1;
+                    //Console.WriteLine(message);
+                }
+                
+            }
+        }
+
+        private static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("{0} messages per second received.", receivedMessages);
+            receivedMessages = 0;
+        }
+    }
+}
---
+++ messaging/zeromq/performance-test/zeromq-perf-producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Edward.Wilde.CSharp.Features.Strings;
+using Edward.Wilde.CSharp.Features.Utilities;
+using NetMQ;
+
+namespace zeromq.perf.producer
+{
+    class Program
+    {
+        static readonly string Message = StringExtensions.RandomString(4.Kilobytes());
+
+        static void Main(string[] args)
+        {
+            const string address = "ipc://127.0.0.1:5556";
+            Console.WriteLine("About to connect to " + address);
+            using (var context = NetMQContext.Create())
+            using (var publisher = context.CreatePublisherSocket())
+            {
+                publisher.Bind(address);                                   
+                int sendMessages = 0;
+                var watch = new Stopwatch();
+                watch.Start();
+                while (true)
+                {
+                    publisher.Send(Message, dontWait: true); // by default subscribers match on begining of message data i.e. key:data, or you can use message envelopes
+                    sendMessages += 1;
+                    if (watch.ElapsedMilliseconds > 1000)
+                    {
+                        Console.WriteLine("{0} messages per second sent.", sendMessages);
+                        watch.Restart();
+                        sendMessages = 0;
+                    }
+                }                
+            }            
+        }
+    }
+}
