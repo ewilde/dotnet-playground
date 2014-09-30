be738724f214c1d47b336c7fbeeac7cefbe512ee
Edward Wilde <ewilde@gmail.com>
2014-09-30 08:35:05 +0100

    Framework utility:IsNet450rNewer

---
+++ programming/csharp-language-features/Utilities/FrameworkUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Edward.Wilde.CSharp.Features.Utilities
+{
+    public static class FrameworkUtility
+    {
+        public static bool IsNet45OrNewer()
+        {
+            // Class "ReflectionContext" exists from .NET 4.5 onwards.
+            return Type.GetType("System.Reflection.ReflectionContext", false) != null;
+        }
+    }
+}
