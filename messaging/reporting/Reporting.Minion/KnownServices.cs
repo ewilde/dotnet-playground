d512813d99c2f79290c05d5e49276f9376f3c284
Edward Wilde <ewilde@gmail.com>
2014-10-18 11:05:42 +0100

    Started work on minion and master

---
+++ messaging/reporting/Reporting.Minion/KnownServices.cs
@@ -0,0 +1,22 @@
+﻿namespace Reporting.Minion
+{
+    public class KnownServices
+    {
+        public const int TibcoConsumerMinionPort = 9101;
+        public const int TibcoProducerMinionPort = 9102;
+
+        public const int RabbitConsumerMinionPort = 9103;
+        public const int RabbitProducerMinionPort = 9104;
+
+        public const int ApolloConsumerMinionPort = 9105;
+        public const int ApolloProducerMinionPort = 9106;
+
+        public const int ZeroMqConsumerMinionPort = 9107;
+        public const int ZeroMqProducerMinionPort = 9108;
+
+        public const int ApacheMqConsumerMinionPort = 9109;
+        public const int ApacheMqProducerMinionPort = 9110;
+
+        public const int ReportingMasterPort = 9999;
+    }
+}
