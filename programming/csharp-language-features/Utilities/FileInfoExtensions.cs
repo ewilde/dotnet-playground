d138a3eed1705968918045fd8be3914a367c07f4
Edward Wilde <ewilde@gmail.com>
2014-11-21 14:03:09 +0000

    Implement ChangeDrive extension for FileInfo

---
+++ programming/csharp-language-features/Utilities/FileInfoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features.Utilities
+{
+    public static class FileInfoExtensions
+    {
+        public static FileInfo ChangeDrive(this FileInfo value, DriveInfo drive)
+        {
+            var oldDrive = new DriveInfo(value.FullName);
+            return new FileInfo(value.FullName.Replace(oldDrive.Name, drive.Name));
+        }
+    }
+
+    [TestFixture]
+    public class FileInfoExtensionsTests
+    {
+        [Test]
+        public void can_swap_drive_letters()
+        {
+           Assert.That(new FileInfo(@"c:\logs\web").ChangeDrive(new DriveInfo("d:")).FullName, Is.EqualTo(new FileInfo(@"d:\logs\web").FullName)); 
+        }
+    }
+}
