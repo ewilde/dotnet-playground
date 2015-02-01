06399c880d337e1aa1709c5d5a31214d7fc312bb
Edward Wilde <ewilde@gmail.com>
2015-02-01 22:39:13 +0000

    implements Fibonacci

--- programming/algorithms/Maths/Fibonacci.cs
+++ programming/algorithms/Maths/Fibonacci.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
+using algorithms.DataStructures;
+using log4net;
 using NUnit.Framework;
 
 namespace algorithms.Maths
 {
     public class Fibonacci
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Fibonacci));
+
+        public Fibonacci()
+        {
+        }
+
         public int CalcuateRecursively(int n)
         {
             return (n < 2) ? n : CalcuateRecursively(n - 1) + CalcuateRecursively(n - 2);
@@ -12,21 +21,33 @@ namespace algorithms.Maths
 
         public int CalculateIteratively(int n)
         {
+            var watch = new Stopwatch();
+            watch.Start();
             int fibPrevious = 0, fibNext = 1;
             for (int i = 2; i <= n; i++)
             {
                 int tmp = fibPrevious;
                 fibPrevious = fibNext;
                 fibNext = tmp + fibNext;
+                //Log.DebugFormat("{0} ticks. [CalculateIteratively]", watch.ElapsedTicks);
             }
-            return (n > 0 ? fibNext : 0);
+
+            var result = (n > 0 ? fibNext : 0);
+            Log.DebugFormat("Completed in {0} ticks. [CalculateIteratively]", watch.ElapsedTicks);
+            return result;
         }
 
-        public int CalculateUsingBinet(int n)
+        public long CalculateUsingBinet(int n)
         {
+            var watch = new Stopwatch();
+            watch.Reset();
+            watch.Start();
             double sqrt5 = Math.Sqrt(5.0);
-            double phi = (1 + sqrt5) / 2;
-            return (int)((Math.Pow(phi, n) - Math.Pow(1 - phi, n)) / sqrt5);
+            double phi = (1 + sqrt5)/2;
+            var result = (long) ((Math.Pow(phi, n) - Math.Pow(1 - phi, n))/sqrt5);
+
+            Log.DebugFormat("Completed in {0} ticks. [CalculateUsingBinet]", watch.ElapsedTicks);
+            return result;
         }
     }
 
@@ -48,7 +69,7 @@ namespace algorithms.Maths
         [Test]
         public void recursive_8th_fib()
         {
-            Assert.That(new Fibonacci().CalculateIteratively(8), Is.EqualTo(21));
+            Assert.That(new Fibonacci().CalcuateRecursively(8), Is.EqualTo(21));
         }
 
         [Test]
@@ -61,6 +82,18 @@ namespace algorithms.Maths
         public void binet_38th_fib()
         {
             Assert.That(new Fibonacci().CalculateUsingBinet(38), Is.EqualTo(39088169));
+        }
+
+        [Test]
+        public void binet_1000th_fib()
+        {
+            Assert.That(new Fibonacci().CalculateUsingBinet(1000), Is.GreaterThan(39088169));
+        }
+
+        [Test]
+        public void iterative_1000th_fib()
+        {
+            Assert.That(new Fibonacci().CalculateIteratively(1000), Is.GreaterThan(39088169));
         }
     }
 }
