cd47396fcaddb04837dcd91a8b03d806f6efdea1
Edward Wilde <ewilde@gmail.com>
2015-02-09 15:39:10 +0000

    implement string format example

---
+++ programming/csharp-language-features/Strings/StringFormatting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features.Strings
+{
+    [TestFixture]
+    public class StringFormatting
+    {
+        [Test]
+        public void Numbers()
+        {
+            Console.WriteLine(string.Format("{0:n}", 6123 / 2.0));      // no decimals
+            Console.WriteLine(string.Format("{0:n0}", 6123 / 2.0));     // decimals
+        }
+    }
+}
