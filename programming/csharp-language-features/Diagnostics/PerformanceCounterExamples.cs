8e398fd03c6fc75d1d03773ec8981e5aa4c4a0b6
Edward Wilde <ewilde@gmail.com>
2015-02-19 15:22:02 +0000

    implement performance counter examples

---
+++ programming/csharp-language-features/Diagnostics/PerformanceCounterExamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using Edward.Wilde.CSharp.Features.Utilities;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features.Diagnostics
+{
+    public static class PerformanceCounterExamples
+    {
+        private static readonly Process CurrentProcess = System.Diagnostics.Process.GetCurrentProcess();
+        public static void MeasurePrivateBytesConsumed()
+        {
+            using (var pc = new PerformanceCounter(
+                    "Process", 
+                    "Private Bytes",
+                    CurrentProcess.ProcessName))
+            {
+                Console.Write(pc.NextValue().ToMegabytes().ToString("n2") + " MB");
+            }
+        }
+    }
+
+    [TestFixture]
+    public class PerformanceCounterExamplesTests
+    {
+        [Test]
+        public void MeasurePrivateBytesConsumedTest()
+        {
+            PerformanceCounterExamples.MeasurePrivateBytesConsumed();
+        }
+    }
+
+}
