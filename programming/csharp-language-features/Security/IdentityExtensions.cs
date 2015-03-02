fd61bbe4ef3664cbd74b7675d426f4249c54c9f8
Edward Wilde <ewilde@gmail.com>
2015-03-02 20:47:37 +0000

    implement IsElevated extension method

---
+++ programming/csharp-language-features/Security/IdentityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Principal;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features.Security
+{
+    public static class IdentityExtensions
+    {
+        public static bool IsElevated(this WindowsIdentity value)
+        {
+            var principal = new WindowsPrincipal(value);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+
+    [TestFixture]
+    public class IdentityExtensionsTests
+    {
+        [Test]
+        public void Is_elevated()
+        {
+            Console.WriteLine(WindowsIdentity.GetCurrent().IsElevated());
+        }
+    }
+}
