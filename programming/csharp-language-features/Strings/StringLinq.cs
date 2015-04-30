fd0c39068f756c3949a56b8f8f0df1a4cf1fce58
Edward Wilde <ewilde@gmail.com>
2015-04-30 07:30:10 +0100

    example of string concat using linq

---
+++ programming/csharp-language-features/Strings/StringLinq.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Edward.Wilde.CSharp.Features.Strings
+{
+    public class StringLinq
+    {
+        public void Concatatention_example_warning_not_performant_obviously()
+        {
+            string[] words = { "one", "two", "three" };
+            var res = words.Aggregate((current, next) => current + ", " + next);
+            Console.WriteLine(res);
+        }
+    }
+}
