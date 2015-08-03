6f4203329629dce087cd2d0c54e30d0c8aae8d6e
Edward Wilde <ewilde@gmail.com>
2015-08-03 21:15:16 +0100

    Add datetime format example

---
+++ programming/csharp-language-features/System/DataTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features
+{
+    [TestFixture]
+    public class DataTimeExamples
+    {
+        [Test]
+        public void Milliseconds()
+        {
+            Console.Write(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fffffff"));
+        }
+    }
+}
