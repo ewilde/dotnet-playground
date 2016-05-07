6267da559bf721b534e132b06b48070927c5c4a3
Edward Wilde <ewilde@gmail.com>
2016-05-07 13:23:28 +0100

    Added HttpClient example

---
+++ programming/csharp-language-features/Http/HttpClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features.Http
+{
+    public class HttpClientExample
+    {
+        static async Task<bool> DownloadPageAsync()
+        {
+            // ... Target page.
+            string page = "http://en.wikipedia.org/";
+
+            // ... Use HttpClient.
+            using (var client = new HttpClient())
+            using (var response = await client.GetAsync(page))
+            using (var content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                // ... Display the result.
+                if (result != null &&
+                result.Length >= 50)
+                {
+                    Console.WriteLine(result.Substring(0, 500) + "...");
+                }
+            }
+
+            return true;
+        }
+
+        [Test]
+        public void TestDownloadPageAsync()
+        {
+            Assert.That(async () => await DownloadPageAsync(), Is.True);
+        }
+    }
+}
