880bd0bbcea8d92e2742cdd43407def14dde6680
Edward Wilde <ewilde@gmail.com>
2016-05-07 14:28:09 +0100

    added json serialize example

---
+++ programming/csharp-language-features/Serialization/JsonSerialization.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Edward.Wilde.CSharp.Features.Serialization
+{
+    public class Employee
+    {
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+    }
+    [TestFixture]
+    public class JsonSerializationExamples
+    {        
+        [Test]
+        public void Serialize()
+        {
+            string json = JsonConvert.SerializeObject(new Employee {FirstName = "Bob", LastName = "Hoskins"}, Formatting.Indented);
+            Debug.WriteLine(json);
+        }
+    }
+}
