5a17759b14e8050f315c790294b9edbb6858c3c7
Edward Wilde <ewilde@gmail.com>
2016-09-13 10:37:24 +0100

    implements IFileSystem

---
+++ programming/csharp-language-features/System/IO/IFileSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edward.Wilde.CSharp.Features.System.IO
+{
+    public interface IFileSystem
+    {
+        bool DirectoryExists(string path);
+        bool FileExists(string path);
+    }
+
+    public class FileSystem : IFileSystem
+    {
+        public bool DirectoryExists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
